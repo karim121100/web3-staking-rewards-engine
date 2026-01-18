ba4a80acd884536e505bad0d67d58c1a47818167
KB <161012310+karim121100@users.noreply.github.com>
2026-01-18 14:32:07 +0600

    Create validator.registry.js

---
+++ validator.registry.js
@@ -0,0 +1,11 @@
+export function loadValidators() {
+  console.log("Loading validators...");
+
+  return [
+    {
+      name: "Validator One",
+      commission: 0.05,
+      totalStaked: 10000
+    }
+  ];
+}
